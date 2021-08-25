2227b65e25a611df0d8344cfd095c224f7333e61
Dmitry Bychenko <Zwerg.CopperNickel@gmail.com>
2021-08-26 00:26:12 +0300

    develop-1.0.0.0 json extensions

---
+++ Json/Jira.Simple.Client.Rest.JsonExtensions.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace Jira.Simple.Client.Json {
+  
+  //-------------------------------------------------------------------------------------------------------------------
+  //
+  /// <summary>
+  /// JSON Element Extensions
+  /// </summary>
+  //
+  //-------------------------------------------------------------------------------------------------------------------
+
+  public static class JsonElementExtensions {
+    #region Constant
+
+    public static readonly JsonElement NullElement = JsonDocument.Parse("null").RootElement;
+
+    #endregion Constant
+
+    #region Public
+
+    #region General
+
+    /// <summary>
+    /// Read
+    /// </summary>
+    /// <param name="parent">Parent Item</param>
+    /// <param name="childrenNames">Names</param>
+    public static JsonElement Read(this JsonElement parent, params string[] childrenNames) {
+      if (childrenNames is null)
+        throw new ArgumentNullException(nameof(childrenNames));
+
+      JsonElement result = parent;
+
+      foreach (string name in childrenNames) 
+        if (!parent.TryGetProperty(name, out result))
+          return NullElement;
+      
+      return result;
+    }
+
+    /// <summary>
+    /// As Enumerable (Arrays only)
+    /// </summary>
+    /// <param name="parent">Parent Item</param>
+    public static IEnumerable<JsonElement> AsEnumerable(this JsonElement parent) {
+      if (parent.ValueKind != JsonValueKind.Array)
+        yield break;
+
+      using var en = parent.EnumerateArray();
+
+      while (en.MoveNext())
+        yield return en.Current;
+    }
+
+    /// <summary>
+    /// Try Get Boolean
+    /// </summary>
+    public static bool TryGetBoolean(this JsonElement parent, out bool result) {
+      if (parent.ValueKind == JsonValueKind.False) {
+        result = false;
+
+        return true;
+      }
+
+      if (parent.ValueKind == JsonValueKind.True) {
+        result = true;
+
+        return true;
+      }
+
+      result = default;
+
+      return false;
+    }
+
+    #endregion General
+
+    #region Reads
+
+    /// <summary>
+    /// Boolean Or Null
+    /// </summary>
+    public static bool? BooleanOrNull(this JsonElement item) {
+      if (item.ValueKind == JsonValueKind.True)
+        return true;
+      if (item.ValueKind == JsonValueKind.False)
+        return false;
+
+      return null;
+    }
+        
+    /// <summary>
+    /// Sbyte Or Null
+    /// </summary>
+    public static sbyte? SbyteOrNull(this JsonElement item) {
+      if (item.ValueKind != JsonValueKind.Number)
+        return null;
+
+      return !item.TryGetSByte(out var result) ? null : result;
+    }
+
+    /// <summary>
+    /// Int16 Or Null
+    /// </summary>
+    public static short? Int16OrNull(this JsonElement item) {
+      if (item.ValueKind != JsonValueKind.Number)
+        return null;
+
+      return !item.TryGetInt16(out var result) ? null : result;
+    }
+
+    /// <summary>
+    /// Int32 Or Null
+    /// </summary>
+    public static int? Int32OrNull(this JsonElement item) {
+      if (item.ValueKind != JsonValueKind.Number)
+        return null;
+
+      return !item.TryGetInt32(out var result) ? null : result;
+    }
+
+    /// <summary>
+    /// Int64 Or Null
+    /// </summary>
+    public static long? Int64OrNull(this JsonElement item) {
+      if (item.ValueKind != JsonValueKind.Number)
+        return null;
+
+      return !item.TryGetInt64(out var result) ? null : result;
+    }
+
+    /// <summary>
+    /// Byte Or Null
+    /// </summary>
+    public static byte? ByteOrNull(this JsonElement item) {
+      if (item.ValueKind != JsonValueKind.Number)
+        return null;
+
+      return !item.TryGetByte(out var result) ? null : result;
+    }
+
+    /// <summary>
+    /// UInt16 Or Null
+    /// </summary>
+    public static ushort? UInt16OrNull(this JsonElement item) {
+      if (item.ValueKind != JsonValueKind.Number)
+        return null;
+
+      return !item.TryGetUInt16(out var result) ? null : result;
+    }
+
+    /// <summary>
+    /// UInt32 Or Null
+    /// </summary>
+    public static uint? UInt32OrNull(this JsonElement item) {
+      if (item.ValueKind != JsonValueKind.Number)
+        return null;
+
+      return !item.TryGetUInt32(out var result) ? null : result;
+    }
+
+    /// <summary>
+    /// UInt64 Or Null
+    /// </summary>
+    public static ulong? UInt64OrNull(this JsonElement item) {
+      if (item.ValueKind != JsonValueKind.Number)
+        return null;
+
+      return !item.TryGetUInt64(out var result) ? null : result;
+    }
+
+    /// <summary>
+    /// Single Or Null
+    /// </summary>
+    public static float? SingleOrNull(this JsonElement item) {
+      if (item.ValueKind != JsonValueKind.Number)
+        return null;
+
+      return !item.TryGetSingle(out var result) ? null : result;
+    }
+
+    /// <summary>
+    /// Double Or Null
+    /// </summary>
+    public static double? DoubleOrNull(this JsonElement item) {
+      if (item.ValueKind != JsonValueKind.Number)
+        return null;
+
+      return !item.TryGetDouble(out var result) ? null : result;
+    }
+
+    /// <summary>
+    /// Decimal Or Null
+    /// </summary>
+    public static Decimal? DecimalOrNull(this JsonElement item) {
+      if (item.ValueKind != JsonValueKind.Number)
+        return null;
+
+      return !item.TryGetDecimal(out var result) ? null : result;
+    }
+
+    /// <summary>
+    /// DateTime Or Null
+    /// </summary>
+    public static DateTime? DateTimeOrNull(this JsonElement item) {
+      if (item.ValueKind != JsonValueKind.String)
+        return null;
+
+      return !item.TryGetDateTime(out var result) ? null : result;
+    }
+
+    /// <summary>
+    /// DateTimeOffset Or Null
+    /// </summary>
+    public static DateTimeOffset? DateTimeOffsetOrNull(this JsonElement item) {
+      if (item.ValueKind != JsonValueKind.String)
+        return null;
+
+      return !item.TryGetDateTimeOffset(out var result) ? null : result;
+    }
+
+    /// <summary>
+    /// DateTimeOffset Or Null
+    /// </summary>
+    public static Guid? GuidOrNull(this JsonElement item) {
+      if (item.ValueKind != JsonValueKind.String)
+        return null;
+
+      return !item.TryGetGuid(out var result) ? null : result;
+    }
+
+    /// <summary>
+    /// String Or Null
+    /// </summary>
+    public static string StringOrNull(this JsonElement item) {
+      if (item.ValueKind == JsonValueKind.String ||
+          item.ValueKind == JsonValueKind.True ||
+          item.ValueKind == JsonValueKind.False ||
+          item.ValueKind == JsonValueKind.Number)
+        return item.ToString();
+
+      return null;
+    }
+
+    #endregion Reads
+
+    #endregion Public
+  }
+
+}
--- Rest/Jira.Simple.Client.Rest.Command.cs
+++ Rest/Jira.Simple.Client.Rest.Command.cs
@@ -8,7 +8,6 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Text.Json;
 using System.Threading;
-using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace Jira.Simple.Client.Rest {
@@ -22,6 +21,12 @@ namespace Jira.Simple.Client.Rest {
   //-------------------------------------------------------------------------------------------------------------------
 
   public sealed class JiraRestCommand {
+    #region Constants
+
+    public const int DEFAULT_PAGE_SIZE = 500;
+
+    #endregion Constants
+
     #region Create
 
     /// <summary>
@@ -53,7 +58,7 @@ namespace Jira.Simple.Client.Rest {
     /// <param name="method">Http Method</param>
     /// <param name="token">Cancellation Token</param>
     /// <returns>Answer Root Element (JSON)</returns>
-    public async Task<JsonElement> QueryAsync(string address, string query, HttpMethod method, CancellationToken token) {
+    public async Task<JsonDocument> QueryAsync(string address, string query, HttpMethod method, CancellationToken token) {
       if (string.IsNullOrEmpty(address))
         throw new ArgumentNullException(nameof(address));
 
@@ -93,7 +98,7 @@ namespace Jira.Simple.Client.Rest {
         .ParseAsync(stream, default, token)
         .ConfigureAwait(false);
 
-      return document.RootElement;
+      return document;
     }
 
     /// <summary>
@@ -103,7 +108,7 @@ namespace Jira.Simple.Client.Rest {
     /// <param name="query">Query (json)</param>
     /// <param name="method">Http Method</param>
     /// <returns>Answer Root Element (JSON)</returns>
-    public async Task<JsonElement> QueryAsync(string address, string query, HttpMethod method) =>
+    public async Task<JsonDocument> QueryAsync(string address, string query, HttpMethod method) =>
       await QueryAsync(address, query, method, CancellationToken.None).ConfigureAwait(false);
 
     /// <summary>
@@ -113,7 +118,7 @@ namespace Jira.Simple.Client.Rest {
     /// <param name="query">Query (json)</param>
     /// <param name="token">Cancellation Token</param>
     /// <returns>Answer Root Element (JSON)</returns>
-    public async Task<JsonElement> QueryAsync(string address, string query, CancellationToken token) =>
+    public async Task<JsonDocument> QueryAsync(string address, string query, CancellationToken token) =>
        await (string.IsNullOrWhiteSpace(query)
         ? QueryAsync(address, null, HttpMethod.Get, token).ConfigureAwait(false)
         : QueryAsync(address, query, HttpMethod.Post, token).ConfigureAwait(false));
@@ -124,7 +129,7 @@ namespace Jira.Simple.Client.Rest {
     /// <param name="address">Address</param>
     /// <param name="query">Query (json)</param>
     /// <returns>Answer Root Element (JSON)</returns>
-    public async Task<JsonElement> QueryAsync(string address, string query) =>
+    public async Task<JsonDocument> QueryAsync(string address, string query) =>
       await (string.IsNullOrWhiteSpace(query)
         ? QueryAsync(address, null, HttpMethod.Get, CancellationToken.None).ConfigureAwait(false)
         : QueryAsync(address, query, HttpMethod.Post, CancellationToken.None).ConfigureAwait(false));
@@ -135,14 +140,14 @@ namespace Jira.Simple.Client.Rest {
     /// <param name="address">Address</param>
     /// <param name="token">Cancellation Token</param>
     /// <returns>Answer Root Element (JSON)</returns>
-    public async Task<JsonElement> QueryAsync(string address, CancellationToken token) =>
+    public async Task<JsonDocument> QueryAsync(string address, CancellationToken token) =>
       await QueryAsync(address, "", HttpMethod.Get, token).ConfigureAwait(false);
 
     /// <summary>
     /// Query
     /// </summary>
     /// <param name="address">Address</param>
-    public async Task<JsonElement> QueryAsync(string address) =>
+    public async Task<JsonDocument> QueryAsync(string address) =>
       await QueryAsync(address, "", HttpMethod.Get, CancellationToken.None).ConfigureAwait(false);
 
     #endregion Query
@@ -158,17 +163,19 @@ namespace Jira.Simple.Client.Rest {
     /// <param name="pageSize"></param>
     /// <param name="token"></param>
     /// <returns></returns>
-    public async IAsyncEnumerable<JsonElement> QueryPagedAsync(string address,
-                                                               string query,
-                                                               HttpMethod method,
-                                                               int pageSize,
-                                                              [EnumeratorCancellation]
-                                                               CancellationToken token) {
+    public async IAsyncEnumerable<JsonDocument> QueryPagedAsync(string address,
+                                                                string query,
+                                                                HttpMethod method,
+                                                                int pageSize,
+                                                               [EnumeratorCancellation]
+                                                                CancellationToken token) {
       if (string.IsNullOrEmpty(address))
         throw new ArgumentNullException(nameof(address));
 
       if (!Connection.IsConnected)
         throw new DataException("Not connected");
+
+      pageSize = pageSize <= 0 ? DEFAULT_PAGE_SIZE : pageSize;
 
       address = string.Join("/", Connection.Server, "rest/api/latest", address.TrimStart('/'));
 
@@ -215,16 +222,128 @@ namespace Jira.Simple.Client.Rest {
             }
           }
 
-          yield return jsonDocument.RootElement;
+          yield return jsonDocument;
 
           startAt += pageSize;
         }
         else {
-          yield return jsonDocument.RootElement;
+          yield return jsonDocument;
 
           startAt = 0;
         }
       }
+    }
+
+    public async IAsyncEnumerable<JsonDocument> QueryPagedAsync(string address,
+                                                                string query,
+                                                                HttpMethod method,
+                                                               [EnumeratorCancellation]
+                                                                CancellationToken token) {
+      await foreach (var item in QueryPagedAsync(address, query, method, DEFAULT_PAGE_SIZE, token))
+        yield return item;
+    }
+
+    public async IAsyncEnumerable<JsonDocument> QueryPagedAsync(string address,
+                                                                string query,
+                                                                HttpMethod method,
+                                                                int pageSize) {
+      await foreach (var item in QueryPagedAsync(address, query, method, pageSize, CancellationToken.None))
+        yield return item;
+    }
+
+    public async IAsyncEnumerable<JsonDocument> QueryPagedAsync(string address,
+                                                                string query,
+                                                                HttpMethod method) {
+      await foreach (var item in QueryPagedAsync(address, query, method, DEFAULT_PAGE_SIZE, CancellationToken.None))
+        yield return item;
+    }
+
+    public async IAsyncEnumerable<JsonDocument> QueryPagedAsync(string address,
+                                                                string query,
+                                                               int pageSize,
+                                                              [EnumeratorCancellation]
+                                                               CancellationToken token) {
+      HttpMethod method = HttpMethod.Post;
+
+      if (string.IsNullOrWhiteSpace(query)) {
+        query = "";
+
+        method = HttpMethod.Get;
+      }
+
+      await foreach (var item in QueryPagedAsync(address, query, method, pageSize, token))
+        yield return item;
+    }
+
+    public async IAsyncEnumerable<JsonDocument> QueryPagedAsync(string address,
+                                                                string query,
+                                                               [EnumeratorCancellation]
+                                                                CancellationToken token) {
+      HttpMethod method = HttpMethod.Post;
+
+      if (string.IsNullOrWhiteSpace(query)) {
+        query = "";
+
+        method = HttpMethod.Get;
+      }
+
+      await foreach (var item in QueryPagedAsync(address, query, method, DEFAULT_PAGE_SIZE, token))
+        yield return item;
+    }
+
+    public async IAsyncEnumerable<JsonDocument> QueryPagedAsync(string address,
+                                                                string query,
+                                                                int pageSize) {
+      HttpMethod method = HttpMethod.Post;
+
+      if (string.IsNullOrWhiteSpace(query)) {
+        query = "";
+
+        method = HttpMethod.Get;
+      }
+
+      await foreach (var item in QueryPagedAsync(address, query, method, pageSize, CancellationToken.None))
+        yield return item;
+    }
+
+    public async IAsyncEnumerable<JsonDocument> QueryPagedAsync(string address,
+                                                                string query) {
+      HttpMethod method = HttpMethod.Post;
+
+      if (string.IsNullOrWhiteSpace(query)) {
+        query = "";
+
+        method = HttpMethod.Get;
+      }
+
+      await foreach (var item in QueryPagedAsync(address, query, method, DEFAULT_PAGE_SIZE, CancellationToken.None))
+        yield return item;
+    }
+
+    public async IAsyncEnumerable<JsonDocument> QueryPagedAsync(string address,
+                                                                int pageSize,
+                                                               [EnumeratorCancellation]
+                                                                CancellationToken token) {
+      await foreach (var item in QueryPagedAsync(address, "", HttpMethod.Get, pageSize, token))
+        yield return item;
+    }
+
+    public async IAsyncEnumerable<JsonDocument> QueryPagedAsync(string address,
+                                                               [EnumeratorCancellation]
+                                                                CancellationToken token) {
+      await foreach (var item in QueryPagedAsync(address, "", HttpMethod.Get, DEFAULT_PAGE_SIZE, token))
+        yield return item;
+    }
+
+    public async IAsyncEnumerable<JsonDocument> QueryPagedAsync(string address,
+                                                                int pageSize) {
+      await foreach (var item in QueryPagedAsync(address, "", HttpMethod.Get, pageSize, CancellationToken.None))
+        yield return item;
+    }
+
+    public async IAsyncEnumerable<JsonDocument> QueryPagedAsync(string address) {
+      await foreach (var item in QueryPagedAsync(address, "", HttpMethod.Get, DEFAULT_PAGE_SIZE, CancellationToken.None))
+        yield return item;
     }
 
     #endregion Paged Query
