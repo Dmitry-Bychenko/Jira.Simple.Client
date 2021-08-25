bd7d0d539234d0288b53c10c1e32e357e104ed12
Dmitry Bychenko <Zwerg.CopperNickel@gmail.com>
2021-08-25 22:12:09 +0300

    Add project files.

---
+++ Rest/Jira.Simple.Client.Rest.Command.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Jira.Simple.Client.Rest {
+  
+  //-------------------------------------------------------------------------------------------------------------------
+  //
+  /// <summary>
+  /// Jira Rest Command
+  /// </summary>
+  //
+  //-------------------------------------------------------------------------------------------------------------------
+
+  public sealed class JiraRestCommand {
+    #region Create
+
+    /// <summary>
+    /// Standard Constructor
+    /// </summary>
+    public JiraRestCommand(JiraRestConnection connection) {
+      Connection = 
+          connection is null ? throw new ArgumentNullException(nameof(connection))
+        : connection.IsDisposed ? throw new ObjectDisposedException(nameof(connection))
+        : connection;
+    }
+
+    #endregion Create
+
+    #region Public
+
+    /// <summary>
+    /// Connection
+    /// </summary>
+    public JiraRestConnection Connection { get; }
+
+    #region Query
+
+    /// <summary>
+    /// Query
+    /// </summary>
+    /// <param name="address">Address</param>
+    /// <param name="query">Query (json)</param>
+    /// <param name="method">Http Method</param>
+    /// <param name="token">Cancellation Token</param>
+    /// <returns>Answer Root Element (JSON)</returns>
+    public async Task<JsonElement> QueryAsync(string address, string query, HttpMethod method, CancellationToken token) {
+      if (string.IsNullOrEmpty(address))
+        throw new ArgumentNullException(nameof(address));
+
+      if (Connection.IsDisposed)
+        throw new ObjectDisposedException(nameof(Connection));
+
+      if (!Connection.IsConnected)
+        throw new DataException("Not connected");
+
+      address = string.Join("/", Connection.Server, "rest/api/latest", address.TrimStart('/'));
+
+      query ??= "{}";
+
+      using var req = new HttpRequestMessage {
+        Method = method,
+        RequestUri = new Uri(address),
+        Headers = {
+          { HttpRequestHeader.Accept.ToString(), "application/json" },
+        },
+        Content = new StringContent(query, Encoding.UTF8, "application/json")
+      };
+
+      var response = await Connection
+        .Client
+        .SendAsync(req, HttpCompletionOption.ResponseHeadersRead, token)
+        .ConfigureAwait(false);
+
+      if (!response.IsSuccessStatusCode)
+        throw new DataException(response.ReasonPhrase);
+
+      using Stream stream = await response
+        .Content
+        .ReadAsStreamAsync(token)
+        .ConfigureAwait(false);
+
+      var document = await JsonDocument
+        .ParseAsync(stream, default, token)
+        .ConfigureAwait(false);
+
+      return document.RootElement;
+    }
+
+    /// <summary>
+    /// Query
+    /// </summary>
+    /// <param name="address">Address</param>
+    /// <param name="query">Query (json)</param>
+    /// <param name="method">Http Method</param>
+    /// <returns>Answer Root Element (JSON)</returns>
+    public async Task<JsonElement> QueryAsync(string address, string query, HttpMethod method) =>
+      await QueryAsync(address, query, method, CancellationToken.None).ConfigureAwait(false);
+
+    /// <summary>
+    /// Query
+    /// </summary>
+    /// <param name="address">Address</param>
+    /// <param name="query">Query (json)</param>
+    /// <param name="token">Cancellation Token</param>
+    /// <returns>Answer Root Element (JSON)</returns>
+    public async Task<JsonElement> QueryAsync(string address, string query, CancellationToken token) =>
+       await (string.IsNullOrWhiteSpace(query)
+        ? QueryAsync(address, null, HttpMethod.Get, token).ConfigureAwait(false)
+        : QueryAsync(address, query, HttpMethod.Post, token).ConfigureAwait(false));
+
+    /// <summary>
+    /// Query
+    /// </summary>
+    /// <param name="address">Address</param>
+    /// <param name="query">Query (json)</param>
+    /// <returns>Answer Root Element (JSON)</returns>
+    public async Task<JsonElement> QueryAsync(string address, string query) =>
+      await (string.IsNullOrWhiteSpace(query)
+        ? QueryAsync(address, null, HttpMethod.Get, CancellationToken.None).ConfigureAwait(false)
+        : QueryAsync(address, query, HttpMethod.Post, CancellationToken.None).ConfigureAwait(false));
+
+    /// <summary>
+    /// Query
+    /// </summary>
+    /// <param name="address">Address</param>
+    /// <param name="token">Cancellation Token</param>
+    /// <returns>Answer Root Element (JSON)</returns>
+    public async Task<JsonElement> QueryAsync(string address, CancellationToken token) =>
+      await QueryAsync(address, "", HttpMethod.Get, token).ConfigureAwait(false);
+
+    /// <summary>
+    /// Query
+    /// </summary>
+    /// <param name="address">Address</param>
+    public async Task<JsonElement> QueryAsync(string address) =>
+      await QueryAsync(address, "", HttpMethod.Get, CancellationToken.None).ConfigureAwait(false);
+
+    #endregion Query
+
+    #region Paged Query
+
+    /// <summary>
+    /// Paged Query
+    /// </summary>
+    /// <param name="address"></param>
+    /// <param name="query"></param>
+    /// <param name="method"></param>
+    /// <param name="pageSize"></param>
+    /// <param name="token"></param>
+    /// <returns></returns>
+    public async IAsyncEnumerable<JsonElement> QueryPagedAsync(string address,
+                                                               string query,
+                                                               HttpMethod method,
+                                                               int pageSize,
+                                                              [EnumeratorCancellation]
+                                                               CancellationToken token) {
+      if (string.IsNullOrEmpty(address))
+        throw new ArgumentNullException(nameof(address));
+
+      if (!Connection.IsConnected)
+        throw new DataException("Not connected");
+
+      address = string.Join("/", Connection.Server, "rest/api/latest", address.TrimStart('/'));
+
+      address += $"{(address.Contains('?') ? '&' : '?')}&maxResults={pageSize}";
+
+      query ??= "{}";
+      int startAt = 0;
+
+      while (startAt >= 0) {
+        using var req = new HttpRequestMessage {
+          Method = method,
+          RequestUri = new Uri(address + $"&startAt={startAt}"),
+          Headers = {
+          { HttpRequestHeader.Accept.ToString(), "application/json" },
+        },
+          Content = new StringContent(query, Encoding.UTF8, "application/json")
+        };
+
+        var response = await Connection
+          .Client
+          .SendAsync(req, HttpCompletionOption.ResponseHeadersRead, token)
+          .ConfigureAwait(false);
+
+        if (!response.IsSuccessStatusCode)
+          throw new DataException(response.ReasonPhrase);
+
+        using Stream stream = await response
+          .Content
+          .ReadAsStreamAsync(token)
+          .ConfigureAwait(false);
+
+        var jsonDocument = await JsonDocument
+          .ParseAsync(stream, default, token)
+          .ConfigureAwait(false);
+
+        if (jsonDocument.RootElement.TryGetProperty("startAt", out var startAtItem)) {
+          using var en = jsonDocument.RootElement.EnumerateObject();
+
+          while (en.MoveNext()) {
+            if (en.Current.Value.ValueKind == JsonValueKind.Array) {
+              if (en.Current.Value.GetArrayLength() <= 0) {
+                yield break;
+              }
+            }
+          }
+
+          yield return jsonDocument.RootElement;
+
+          startAt += pageSize;
+        }
+        else {
+          yield return jsonDocument.RootElement;
+
+          startAt = 0;
+        }
+      }
+    }
+
+    #endregion Paged Query
+
+    #endregion Public
+  }
+
+}
---
+++ Rest/Jira.Simple.Client.Rest.Connection.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Jira.Simple.Client.Rest {
+  
+  //-------------------------------------------------------------------------------------------------------------------
+  //
+  /// <summary>
+  /// Jira Rest Connection
+  /// </summary>
+  //
+  //-------------------------------------------------------------------------------------------------------------------
+
+  public sealed class JiraRestConnection : IDisposable, IEquatable<JiraRestConnection> {
+    #region Private Data
+
+    private static readonly IReadOnlyDictionary<char, string> s_Escape = new Dictionary<char, string>() {
+      { '\\', "\\\\" },
+      { '"', "\\\"" },
+      { '\n', "\\n" },
+      { '\r', "\\r" },
+      { '\t', "\\t" },
+      { '\f', "\\f" },
+      { '\b', "\\b" },
+    };
+
+    private CookieContainer m_CookieContainer;
+
+    private HttpClient m_HttpClient;
+
+    #endregion Private Data
+
+    #region Algorithm
+
+    private static string Escape(string value) {
+      if (value is null)
+        return "null";
+
+      StringBuilder sb = new(value.Length * 2);
+
+      foreach (char c in value)
+        sb.Append(s_Escape.TryGetValue(c, out var s) ? s : c);
+
+      return sb.ToString();
+    }
+
+    private void CoreCreateClient() {
+      try {
+        ServicePointManager.SecurityProtocol =
+          SecurityProtocolType.Tls |
+          SecurityProtocolType.Tls11 |
+          SecurityProtocolType.Tls12;
+      }
+      catch (NotSupportedException) {
+        ;
+      }
+
+      m_CookieContainer = new CookieContainer();
+
+      var handler = new HttpClientHandler() {
+        CookieContainer = m_CookieContainer,
+        Credentials = CredentialCache.DefaultCredentials,
+      };
+
+      m_HttpClient = new HttpClient(handler) {
+        Timeout = Timeout.InfiniteTimeSpan,
+      };
+    }
+
+    private async Task CoreConnectAsync(CancellationToken token) {
+      string query =
+         @$"{{
+             ""username"": ""{Escape(Login)}"",
+             ""password"": ""{Escape(Password)}""
+           }}";
+
+      using var req = new HttpRequestMessage {
+        Method = HttpMethod.Post,
+        RequestUri = new Uri(string.Join('/', Server, "rest/auth/1/session")),
+        Headers = {
+          { HttpRequestHeader.Accept.ToString(), "application/json" },
+        },
+        Content = new StringContent(query, Encoding.UTF8, "application/json")
+      };
+
+      var response = await m_HttpClient
+        .SendAsync(req, HttpCompletionOption.ResponseHeadersRead, token)
+        .ConfigureAwait(false);
+
+      if (!response.IsSuccessStatusCode)
+        throw new DataException($"Failed to Connect to Jira: {response.ReasonPhrase}");
+    }
+
+    #endregion Algorithm
+
+    #region Create
+
+    /// <summary>
+    /// Standard Constructor
+    /// </summary>
+    /// <param name="login">Login</param>
+    /// <param name="password">Password</param>
+    /// <param name="server">Server, e.g. https://jira-my-server.com</param>
+    public JiraRestConnection(string login, string password, string server) {
+      Login = login ?? throw new ArgumentNullException(nameof(login));
+      Password = password ?? throw new ArgumentNullException(nameof(password));
+      Server = server?.TrimEnd(' ', '/') ?? throw new ArgumentNullException(nameof(server));
+
+      CoreCreateClient();
+    }
+
+    #endregion Create
+
+    #region Public
+
+    /// <summary>
+    /// Http Client
+    /// </summary>
+    public HttpClient Client => m_HttpClient;
+
+    /// <summary>
+    /// Login
+    /// </summary>
+    public string Login { get; }
+
+    /// <summary>
+    /// Password
+    /// </summary>
+    public string Password { get; }
+
+    /// <summary>
+    /// Server
+    /// </summary>
+    public string Server { get; }
+
+    /// <summary>
+    /// Session Id
+    /// </summary>
+    public string SessionId {
+      get {
+        if (!IsConnected || IsDisposed)
+          return null;
+
+        var cookies = m_CookieContainer.GetCookies(new Uri(Server));
+
+        return cookies["JSessionID"]?.Value;
+      }
+    }
+
+    /// <summary>
+    /// Token
+    /// </summary>
+    public string Token {
+      get {
+        if (!IsConnected || IsDisposed)
+          return null;
+
+        var cookies = m_CookieContainer.GetCookies(new Uri(Server));
+
+        return cookies["atlassian.xsrf.token"]?.Value;
+      }
+    }
+
+    /// <summary>
+    /// Is Connected
+    /// </summary>
+    public bool IsConnected { get; private set; }
+
+    /// <summary>
+    /// Connect Async
+    /// </summary>
+    public async Task ConnectAsync(CancellationToken token) {
+      if (IsDisposed)
+        throw new ObjectDisposedException("this");
+
+      if (IsConnected)
+        return;
+
+      await CoreConnectAsync(token).ConfigureAwait(false);
+
+      IsConnected = true;
+    }
+
+    /// <summary>
+    /// Connect Async
+    /// </summary>
+    public async Task ConnectAsync() => await ConnectAsync(CancellationToken.None);
+
+    /// <summary>
+    /// To String
+    /// </summary>
+    public override string ToString() => $"{Login}@{Server}{(IsConnected ? " (connected)" : "")}";
+
+    #endregion Public
+
+    #region IDisposable
+
+    /// <summary>
+    /// Is Disposed
+    /// </summary>
+    public bool IsDisposed { get; private set; }
+
+    // Dispose
+    private void Dispose(bool disposing) {
+      if (IsDisposed)
+        return;
+
+      if (disposing) {
+        m_HttpClient.Dispose();
+
+        IsConnected = false;
+        IsDisposed = true;        
+      }
+    }
+
+    /// <summary>
+    /// Dispose
+    /// </summary>
+    public void Dispose() => Dispose(true);
+
+    #endregion IDisposable
+
+    #region IEquatable<JiraRestConnection>
+
+    /// <summary>
+    /// Equals
+    /// </summary>
+    public bool Equals(JiraRestConnection other) {
+      if (ReferenceEquals(this, other))
+        return true;
+      if (other is null)
+        return true;
+
+      return IsConnected == other.IsConnected &&
+             IsDisposed == other.IsDisposed &&
+             string.Equals(Server, other.Server, StringComparison.OrdinalIgnoreCase) &&
+             string.Equals(Login, other.Login, StringComparison.OrdinalIgnoreCase);
+    }
+
+    /// <summary>
+    /// Equals
+    /// </summary>
+    public override bool Equals(object obj) => obj is JiraRestConnection other && Equals(other);
+
+    /// <summary>
+    /// Hash Code
+    /// </summary>
+    public override int GetHashCode() =>
+      Server.GetHashCode(StringComparison.OrdinalIgnoreCase) ^ Login.GetHashCode(StringComparison.OrdinalIgnoreCase);
+
+    #endregion IEquatable<JiraRestConnection>
+  }
+
+}
